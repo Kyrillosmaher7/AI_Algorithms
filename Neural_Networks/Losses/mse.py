358da66369d88cceaebde8945b056c854b249c8c
Kyrillosmaher7 <kirolos7maher@gmail.com>
2026-07-03 14:04:44 +0300

    Implement Most popular Loss Functions Like [MAE,MSE,huber,cross_entropy]

---
+++ Neural_Networks/Losses/mse.py
@@ -0,0 +1,22 @@
+import numpy as np
+from Neural_Networks.Losses.Loss import Loss
+
+
+class MSELoss(Loss):
+    """
+    Mean Squared Error (MSE) Loss function.
+    Formula: L(y_true, y_pred) = mean((y_true - y_pred)^2)
+    Used for regression problems to measure the average squared difference between true and predicted values.
+    Choose MSE when:
+        - Large prediction errors are especially costly.
+        - You want smoother gradients for optimization.
+        - You're training neural networks or most regression models.
+    """
+    def __init__(self):
+        super().__init__("Mean Squared Error Loss")
+
+    def forward(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.mean((y_true - y_pred) ** 2)
+
+    def backward(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        return 2 * (y_pred - y_true) / y_true.size
